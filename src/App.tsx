b2fcac2db251742df544b2859886ebcd1a0daaec
ks145KM <sutokota0522@outlook.jp>
2024-11-29 11:59:46 +0900

    出力テキストエリアの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import InputTextArea from './components/InputTextArea'
-import OutputTextArea from './components/OutputTextArea';
 import { convertJsonToReactComponent } from './utils/ConvertJsonToReactComponent';
 
 
 function App() {
-  //const [output, setOutput] = useState("");
 
   const handleConvert = (inputText: string) => {
     //TODO:変換処理を書く
     console.log(convertJsonToReactComponent(inputText));
 
-    //reactConponentを文字列として渡す reactComponentString
-    // setOutput(reactComponentString);
   };
 
   return (
     <div>
       <h1>JSON to React Component</h1>
       <InputTextArea onConvert={handleConvert} />
-      <OutputTextArea output={""}/>
     </div>
   )
 
