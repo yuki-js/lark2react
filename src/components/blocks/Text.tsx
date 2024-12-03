af911dbfcd708cacf37e393cf0f9cb28bfb82330
ks145KM <sutokota0522@outlook.jp>
2024-12-03 20:16:10 +0900

    Textコンポーネントの追加

--- src/components/blocks/Text.tsx
+++ src/components/blocks/Text.tsx
@@ -1,4 +1,72 @@
+import { css } from "@emotion/react";
+import { FONT_COLOR } from "../../design/fontColor";
+import { FONT_BACKGROUND_COLOR } from "../../design/fontBackgroundColor";
+
 export function Text({ blockData, hash }) {
-  const title = blockData.text.elements[0].text_run.content;
-  return <div>{title}</div>;
+
+  const elements = blockData.text.elements;
+
+  return (
+    <div>
+      {elements.map((element, index) => {
+        const elementTextColor = element.text_run.text_element_style.text_color;
+        const elementBackgroundColor =
+          element.text_run.text_element_style.background_color;
+
+        const fontColor = elementTextColor
+          ? FONT_COLOR[elementTextColor]
+          : "black";
+        const backgroundColor = elementBackgroundColor
+          ? FONT_BACKGROUND_COLOR[elementBackgroundColor]
+          : "transparent";
+
+        const bold = element.text_run.text_element_style.bold
+          ? "bold"
+          : "normal";
+
+        {/* todo inline codeでは、ボーダーラインなども表示させた方がいい*/}
+        const inlineCode = element.text_run.text_element_style.inline_code
+          ? "monospace"
+          : "inherit";
+        
+          const italic = element.text_run.text_element_style.italic
+          ? "italic"
+          : "normal";
+
+        const strikeThrough = element.text_run.text_element_style.strikethrough
+          ? "line-through"
+          : "none";
+        const underline = element.text_run.text_element_style.underline
+          ? "underline"
+          : "none";
+
+        const decoration =
+          [
+            strikeThrough === "line-through" && "line-through",
+            underline === "underline" && "underline",
+          ]
+            .filter(Boolean)
+            .join(" ") || "none";
+
+        const cssStyle = css({
+          color: fontColor,
+          fontWeight: bold,
+          fontFamily: inlineCode,
+          fontStyle: italic,
+          textDecoration: decoration,
+          backgroundColor: backgroundColor,
+          display: "inline-block",
+        });
+
+        return(
+          <div key={index} css={cssStyle}>
+            {element.text_run.content}
+          </div>
+        );
+      })}
+    </div>
+  );
+
+
+  
 }
