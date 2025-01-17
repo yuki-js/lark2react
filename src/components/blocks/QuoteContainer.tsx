3791efb7cc1265b8682004efa03b564c9b379e61
ks145KM <sutokota0522@outlook.jp>
2025-01-17 18:09:59 +0900

    QuoteContainerブロックの実装

--- src/components/blocks/QuoteContainer.tsx
+++ src/components/blocks/QuoteContainer.tsx
@@ -1,12 +1,20 @@
 import { css } from "@emotion/react";
+import { id2Component, displayChildComponent } from "../../utils/utils";
 import { FONT_COLOR } from "../../styles/fontColor";
 import { FONT_BACKGROUND_COLOR } from "../../styles/fontBackgroundColor";
 import { generateTextStyle } from "../../utils/utils";
 
 
-export function QuoteContainer({ blockDataArr, hash }) {
+export function QuoteContainer({ blockData, hash }) {
+
+    const cssStyle = css({
+        borderLeft: "4px solid lightgray",
+        paddingLeft: "16px",
+    });
 
     return(
-        <div>QuoteContainer</div>
+        <div css={cssStyle}>
+            {displayChildComponent(blockData, hash)}
+        </div>
     )
 }
