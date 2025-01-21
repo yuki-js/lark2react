74f28afa3ed6187ab90ee67ce037f6d75f7828c2
ks145KM <sutokota0522@outlook.jp>
2025-01-21 21:00:30 +0900

    calloutブロック 実装(絵文字表示はさせていない)

--- src/components/blocks/Callout.tsx
+++ src/components/blocks/Callout.tsx
@@ -1,5 +1,21 @@
 import { css } from "@emotion/react";
+import { id2Component, displayChildComponent } from "../../utils/utils";
+import { CALLOUT_BACKGROUND_COLOR } from "../../styles/calloutBackgroundColor";
+import { CALLOUT_BORDER_COLOR } from "../../styles/calloutBorderColor";
+import { generateTextStyle } from "../../utils/utils";
 
 export function Callout({ blockData, hash }) {
-  return  (<div>callout</div>);
+
+    const style = blockData.callout
+
+    const boxStyle = css({
+        backgroundColor:  CALLOUT_BACKGROUND_COLOR[style.background_color],
+        border:  `2px solid ${CALLOUT_BORDER_COLOR[style.border_color]}`,
+        borderRadius: "8px",
+        padding: "15px",
+        marginTop: "10px",
+        marginBottom: "10px",
+      });
+
+  return <div css={boxStyle}>{displayChildComponent(blockData, hash)}</div>;
 }
--- src/styles/calloutBackgroundColor.ts
+++ src/styles/calloutBackgroundColor.ts
@@ -1,16 +1,16 @@
 export const CALLOUT_BACKGROUND_COLOR = {
-  1: "lightred",
-  2: "lightorange",
-  3: "lightyellow",
-  4: "lightgreen",
-  5: "lightblue",
-  6: "lightpurple",
-  7: "lightgray",
-  8: "darkred",
-  9: "darkorange",
-  10: "darkyellow",
-  11: "darkgreen",
-  12: "darkblue",
-  13: "darkpurple",
-  14: "darkgray",
+  1: "#FF6347", // Tomato as a valid color code since 'lightred' is not standard
+  2: "#FFA07A", // Light salmon as a valid color code since 'lightorange' is not standard
+  3: "lightyellow", // This is a valid CSS color name
+  4: "lightgreen", // This is a valid CSS color name
+  5: "lightblue", // This is a valid CSS color name
+  6: "plum", // 'lightpurple' is not standard, but 'plum' is a light purple color
+  7: "lightgray", // This is a valid CSS color name
+  8: "#8B0000", // Dark red as a valid color code
+  9: "#FF8C00", // Dark orange as a valid color code
+  10: "#FFD700", // Gold as a valid color code since 'darkyellow' is not standard
+  11: "darkgreen", // This is a valid CSS color name
+  12: "darkblue", // This is a valid CSS color name
+  13: "indigo", // 'darkpurple' is not standard, but 'indigo' is a dark purple color
+  14: "darkgray", // This is a valid CSS color name
 };
