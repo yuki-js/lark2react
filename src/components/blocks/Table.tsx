f7b66ad006f8a1f0e50e670aaabac876f29e4515
ks145KM <sutokota0522@outlook.jp>
2025-04-10 15:19:43 +0900

    Tableコンポーネントのプロパティ変数を記述

--- src/components/blocks/Table.tsx
+++ src/components/blocks/Table.tsx
@@ -2,9 +2,17 @@ import { BlockInnerComponent } from "../../types";
 import { BlockComponent } from "../BlockComponent";
 
 export const Table: BlockInnerComponent = ({ block }) => {
+
+  const rowSize = block.table?.property.row_size;
+  const columnSize = block.table?.property.column_size;
+  const columnWidth = block.table?.property.column_width; //array of numbers
+  const headerRow = block.table?.property.header_row;
+  const headerColumn = block.table?.property.header_column;
+  const mergeInfo = block.table?.property.merge_info;
+
   return (
     <div>
-      {block.children?.map((childId) => (
+      {block.table?.cells?.map((childId) => (
         <BlockComponent key={childId} blockId={childId} />
       ))}
     </div>
--- src/components/blocks/TableCell.tsx
+++ src/components/blocks/TableCell.tsx
@@ -2,9 +2,10 @@ import { BlockInnerComponent } from "../../types";
 import { BlockComponent } from "../BlockComponent";
 
 export const TableCell: BlockInnerComponent = ({ block }) => {
+
   return(
     <div>
-      {block.children?.map((childId) => (
+      {block.children.map((childId) => (
         <BlockComponent key={childId} blockId={childId} />
       ))}
     </div>
